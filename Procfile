6399a398800b69d5ac5ce483bb90f37700a16f24
dem19 <dem19vieira@gmail.com>
2019-07-04 22:54:58 -0300

    sistema de chave

---
+++ Procfile
@@ -0,0 +1,3 @@
+web: gunicorn chave.wsgi --log-file -
+release: python manage.py migrate --noinput
+
--- core/views.py
+++ core/views.py
@@ -27,12 +27,12 @@ def mostrar_chaves_disponiveis(request):
     return render(request, 'mostrar_chaves_disponiveis.html',{'list':list})
 
 def mostrar_chaves_indisponiveis(request):
-    list = Chave.objects.filter(disponivel=False)
+    list = Emprestimo.objects.filter(user=request.user.id)
     return render(request, 'mostrar_chaves_indisponiveis.html',{'list':list})
 
 @login_required
 def pegar_chave(request, id):
-    list = Chave.objects.filter
+    list = Chave.objects.filter(disponivel=True)
     form = FormMostrarQuemPegou(request.POST or None)
     if form.is_valid():
         alterar = Chave.objects.filter(id=id).update(disponivel=False)
@@ -40,17 +40,28 @@ def pegar_chave(request, id):
         return redirect(home)
     return render(request, 'pegar_chave.html',{'form':form, 'list':list})
 
+##########################################
+
 @login_required
 def devolver_chave(request,id):
-    list2 = Emprestimo.objects.filter(user=request.user.id)
-    form = FormMostrarQuemDevolveu(request.POST or None)
+    list2 = Emprestimo.objects.get(id=id)
+    form = FormMostrarQuemDevolveu(request.POST or None, instance=list2)
+    dev = FormMostrarQuemDevolveu(request.POST or None)
     if form.is_valid():
         alterar = Chave.objects.filter(id=id).update(disponivel=True)
         form.save()
+        dev.save()
         return redirect(home)
-    return render(request, 'devolver_chave.html',{'form2':form, 'list2':list2})
+    return render(request, 'devolver_chave.html',{'form':form})
 
+
+###########################################
 def historico_chave(request):
-    list = Emprestimo.objects.filter(user=request.user.id)
-    list2 = Devolver.objects.filter(user=request.user.id)
+    list = Emprestimo.objects.all()
+    list2 = Devolver.objects.all()
     return render(request, 'historico_chave.html',{'list': list, 'list2': list2})
+
+
+
+
+
